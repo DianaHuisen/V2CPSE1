b768028071757bedde039d4e429d02c6cb706acb
Diana <43001634+DianaHuisen@users.noreply.github.com>
2019-09-18 10:27:48 -0700

    Add files via upload

---
+++ Opdrachten Week 1/Opdracht 1.1/old.asm
@@ -0,0 +1,14 @@
+.cpu cortex-m0
+.text
+.align 2
+.global print_asciz
+
+print_asciz:    PUSH { R4, LR }
+                MOV R4, R0
+loop:           LDRB R0, [ R4 ]
+                TST R0, #0
+                BEQ done
+                BL uart_put_char
+                ADD R4, R4, #1
+                B loop
+done:           POP { R4, PC }
