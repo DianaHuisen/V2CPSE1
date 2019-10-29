840e613439e13cfbd56da5ca098b5b0a1c6bcc9e
Diana <43001634+DianaHuisen@users.noreply.github.com>
2019-10-29 07:24:35 -0700

    working & tested

---
+++ Week2/2.7(updated)/meldody.cpp
@@ -0,0 +1,20 @@
+#include "melody.hpp" 
+void meldody::play( note_player &p){
+	 p.play( note{880,1000000} );
+	 p.play( note{0,1000000} );
+	 p.play( note{880,1000000} );
+	 p.play( note{0,1000000} );
+	 p.play( note{880,1000000} );
+	 p.play( note{0,1000000} );
+	 p.play( note{880,2000000} );
+	 p.play( note{0,1000000} );
+	 p.play( note{880,2000000} );
+	 p.play( note{0,1000000} );
+	 p.play( note{880,2000000} );
+	 p.play( note{0,1000000} );
+	 p.play( note{880,1000000} );
+	 p.play( note{0,1000000} );
+	 p.play( note{880,1000000} );
+	 p.play( note{0,1000000} );
+	 p.play( note{880,1000000} );
+}
---
+++ Week2/2.7(updated)/my_player.cpp
@@ -0,0 +1,8 @@
+#include "my_player.hpp"
+//#include <iostream>
+#include <fstream>
+
+void my_player::play( const note & n ){
+//    meldody << "\t p.play( note{"<<n.duration<<"," <<n.frequency<<"} );\n";
+    meldody << "\t p.play( note{"<<n.frequency<<"," <<n.duration<<"} );\n";
+}
