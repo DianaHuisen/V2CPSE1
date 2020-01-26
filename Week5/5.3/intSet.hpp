62c4b65d61f26175f1c7ae0d5d54ebf9a6ef4a30
Diana <43001634+DianaHuisen@users.noreply.github.com>
2020-01-27 00:35:48 +0100

    Add files via upload

---
+++ Week5/5.3/intSet.hpp
@@ -0,0 +1,67 @@
+#ifndef _INTSET_HPP
+#define _INTSET_HPP
+
+#include <iostream>
+#include <array>
+
+template<typename T, int N>
+
+class intSet{
+private:
+    std::array<T, N> setInt;
+    int counter = 0;
+    
+public:
+    intSet(){
+        for (unsigned int i =0; i<=N; i++){
+            setInt[i]=0;
+        }
+    }
+    void add(T n){
+        if (counter<N && (contains(n)==false || n==0)){
+            setInt[counter]= n;
+            counter++;
+        }
+    }
+    void remove(T n){
+        if(contains(n)==true){
+            for(int i=0; i<counter; i++){
+                if (setInt[i] == n){
+                    while(i<counter){
+                        setInt[i]=setInt[i+1];
+                        i++;
+                    }            
+                    if (i == counter){
+                        setInt[i-1]=0;
+                    }
+                }
+            }
+        counter--;
+        }
+    }
+    bool contains(T n){
+        for(unsigned int i=0; i<N; i++){
+            if (setInt[i] == n){
+                return true;
+            }
+        }
+        return false;
+    }
+    friend std::ostream & operator<<(std::ostream & lhs, const intSet & rhs){
+        for(unsigned int i=0; i<N; i++){
+            lhs << rhs.setInt[i] <<" ";
+        }
+        return lhs;
+    }
+    T max(){
+        T highest =0;
+        for(unsigned int i=0; i<N; i++){
+            if (setInt[i] > highest){
+                highest = setInt[i];
+            }
+        }
+        return highest;
+    }
+};
+
+#endif
---
+++ Week5/5.3/main.cpp
@@ -0,0 +1,102 @@
+#define CATCH_CONFIG_MAIN
+
+#include "intSet.hpp"
+#include <iostream>
+#include "catch.hpp"
+
+
+TEST_CASE("add, double, max"){
+    intSet <int, 10> lijst;
+    lijst.add(1);
+    lijst.add(1);
+    std::stringstream test;
+    test << lijst;
+    REQUIRE(test.str() == "1 0 0 0 0 0 0 0 0 0 ");
+    std::stringstream test1;
+    test1 << lijst.max();
+    REQUIRE(test1.str() == "1");
+}
+
+TEST_CASE("add, zero and overflow, max"){
+    intSet <int, 10> lijst;
+    lijst.add(0);
+    lijst.add(1);
+    lijst.add(2);
+    lijst.add(3);
+    lijst.add(4);
+    lijst.add(5);
+    lijst.add(6);
+    lijst.add(7);
+    lijst.add(8);
+    lijst.add(9);
+    lijst.add(10);
+    std::stringstream test;
+    test << lijst;
+    REQUIRE(test.str() == "0 1 2 3 4 5 6 7 8 9 ");
+    std::stringstream test1;
+    test1 << lijst.max();
+    REQUIRE(test1.str() == "9");
+}
+
+
+TEST_CASE("float, add, double, max"){
+    intSet <float, 10> lijst;
+    lijst.add(1);
+    lijst.add(1);
+    std::stringstream test;
+    test << lijst;
+    REQUIRE(test.str() == "1 0 0 0 0 0 0 0 0 0 ");
+    std::stringstream test1;
+    test1 << lijst.max();
+    REQUIRE(test1.str() == "1");
+}
+
+TEST_CASE("float, add, zero and overflow, max"){
+    intSet <float, 10> lijst;
+    lijst.add(0);
+    lijst.add(1);
+    lijst.add(2);
+    lijst.add(3);
+    lijst.add(4);
+    lijst.add(5);
+    lijst.add(6);
+    lijst.add(7);
+    lijst.add(8.6);
+    lijst.add(9);
+    lijst.add(10);
+    std::stringstream test;
+    test << lijst;
+    REQUIRE(test.str() == "0 1 2 3 4 5 6 7 8.6 9 ");
+    std::stringstream test1;
+    test1 << lijst.max();
+    REQUIRE(test1.str() == "9");
+}
+
+TEST_CASE("float5, add, double, max"){
+    intSet <float, 5> lijst;
+    lijst.add(1);
+    lijst.add(1);
+    std::stringstream test;
+    test << lijst;
+    REQUIRE(test.str() == "1 0 0 0 0 ");
+    std::stringstream test1;
+    test1 << lijst.max();
+    REQUIRE(test1.str() == "1");
+}
+
+TEST_CASE("float5, add, zero and overflow, max"){
+    intSet <float, 5> lijst;
+    lijst.add(0);
+    lijst.add(1);
+    lijst.add(2.2);
+    lijst.add(3.0);
+    lijst.add(4.0);
+    lijst.add(5.0);
+    lijst.add(8);
+    std::stringstream test;
+    test << lijst;
+    REQUIRE(test.str() == "0 1 2.2 3 4 ");
+    std::stringstream test1;
+    test1 << lijst.max();
+    REQUIRE(test1.str() == "4");
+}
